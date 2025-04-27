23e21cfa6674cfe8082549ca1d43eb7b62755645
abdumanonov <abdumannonovabdujabbor5@gmail.com>
2025-04-27 16:51:09 +0500

    vuex actions

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -8,7 +8,15 @@ const mutations = {
   },
 };
 
+const actions = {
+  register(context) {
+    setTimeout(() => {
+      context.commit("setLoading");
+    }, 2000);
+  },
+};
 export default {
   state,
   mutations,
+  actions,
 };
